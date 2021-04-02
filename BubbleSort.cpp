3f1d79c6d9131f24042b5d0af313a2b20f647aa1
hthompso15 <hthompso@uvm.edu>
2021-04-02 15:00:21 -0400

    Did some TODOs in cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -14,16 +14,62 @@ int main(int argc, char* argv[]) {
         size = 10000;
     }
     cout << "Looking for size: " << size << endl;
+
+    vector<long> cVec;
     
-    // TODO: Declare a vector of longs to store the numbers
-    
-    // TODO: Read size numbers from numbers.txt
-    
-    // TODO: Print the vector size (to make sure it matches the size printed above)
-    
-    // TODO: Bubble Sort the vector
-    
+    ifstream fIn;
+    fIn.open("numbers.txt");
+
+    for (int i = 0; i < size; i++) {
+        int num;
+        fIn >> num;
+        cVec.push_back(num);
+        
+    }
+
+    cout << "Vector size: " << cVec.size() << endl;
+
+    // Fix Bubble sort the vector
+    bool swapped = true;
+    int maxIndex = cVec.size()-1;
+    while (swapped) {
+        swapped = false;
+        for(int i; i < maxIndex; i++) {
+            if (cVec[i+1] < cVec[i]) {
+                int temp = cVec[i];
+                cVec[i] = cVec[i+1];
+                cVec[i+1] = temp;
+                swapped = true;
+            }
+        }
+        maxIndex -= 1;
+    }
+
     // TODO: Print the first and last ten numbers from the vector to the console
 
+    cout << "First 10 Numbers from Vector:" << endl;
+
+    for (int i = 0; i < 10; i++) {
+        cout << cVec[i] << endl;
+    }
+
+    cout << "Last 10 Numbers from Vector:" << endl;
+
+    for (int i = cVec.size(); i == -10; i--) {
+        cout << cVec[i] << endl;
+    }   
+
     return 0;
+
 }
+
+
+ 
+
+
+
+
+
+
+
+
